775b0f6e31d0b00652bdd346460e25630dcdf919
hultenvp <61835400+hultenvp@users.noreply.github.com>
2021-08-29 16:49:14 +0200

    Update sensor.py

--- custom_components/solis/sensor.py
+++ custom_components/solis/sensor.py
@@ -12,71 +12,27 @@ import struct
 import sys
 import voluptuous as vol
 
+from .const import (
+  VERSION,
+  CONF_PORTAL_DOMAIN,
+  CONF_USERNAME,
+  CONF_PASSWORD,
+  CONF_PLANT_ID,
+  CONF_INVERTER_SERIAL,
+  CONF_SENSORS,
+  SENSOR_PREFIX,
+  DEFAULT_DOMAIN, 
+  SENSOR_TYPES,
+)
 from datetime import datetime
 from datetime import timedelta
-from homeassistant.components.sensor import (
-    PLATFORM_SCHEMA,
-    STATE_CLASS_MEASUREMENT,
-    STATE_CLASS_TOTAL_INCREASING,
-)
-from homeassistant.const import ( 
-    EVENT_HOMEASSISTANT_STOP, 
-    CONF_NAME, 
-    CONF_SCAN_INTERVAL, 
-    TEMP_CELSIUS,
-    DEVICE_CLASS_CURRENT,
-    DEVICE_CLASS_ENERGY,
-    DEVICE_CLASS_POWER,
-    DEVICE_CLASS_VOLTAGE,
-    DEVICE_CLASS_TEMPERATURE,)
 from homeassistant.core import callback
 from homeassistant.helpers.entity import Entity
-
 
 from .platform2_portal import InverterData as inverter
 from .platform2_portal import PortalConfig as inverter_config
 
-# VERSION
-VERSION = '0.2.0'
-
 _LOGGER = logging.getLogger(__name__)
-
-CONF_PORTAL_DOMAIN = 'portal_domain'
-CONF_USERNAME = 'portal_username'
-CONF_PASSWORD = 'portal_password'
-CONF_PLANT_ID = 'portal_plant_id'
-CONF_INVERTER_SERIAL = 'inverter_serial'
-CONF_SENSORS = 'sensors'
-
-SENSOR_PREFIX = 'Solis'
-DEFAULT_DOMAIN = 'm.ginlong.com'
-
-# Supported sensor types:
-# Key: ['label', unit, icon, device class, state class]
-SENSOR_TYPES = {
-    'status':            ['Status', None, 'mdi:solar-power', None, None],
-    'temperature':       ['Temperature', TEMP_CELSIUS, 'mdi:thermometer', DEVICE_CLASS_TEMPERATURE, STATE_CLASS_MEASUREMENT],
-    'dcinputvoltagepv1': ['DC Voltage PV1', 'V', 'mdi:flash-outline', DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT],
-    'dcinputvoltagepv2': ['DC Voltage PV2', 'V', 'mdi:flash-outline', DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT],
-    'dcinputvoltagepv3': ['DC Voltage PV3', 'V', 'mdi:flash-outline', DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT],
-    'dcinputvoltagepv4': ['DC Voltage PV4', 'V', 'mdi:flash-outline', DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT],
-    'dcinputcurrentpv1': ['DC Current PV1', 'A', 'mdi:flash-outline', DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT],
-    'dcinputcurrentpv2': ['DC Current PV2', 'A', 'mdi:flash-outline', DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT],
-    'dcinputcurrentpv3': ['DC Current PV3', 'A', 'mdi:flash-outline', DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT],
-    'dcinputcurrentpv4': ['DC Current PV4', 'A', 'mdi:flash-outline', DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT],
-    'acoutputvoltage1':  ['AC Voltage R', 'V', 'mdi:flash-outline', DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT],
-    'acoutputvoltage2':  ['AC Voltage S', 'V', 'mdi:flash-outline', DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT],
-    'acoutputvoltage3':  ['AC Voltage T', 'V', 'mdi:flash-outline', DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT],
-    'acoutputcurrent1':  ['AC Current R', 'A', 'mdi:flash-outline', DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT],
-    'acoutputcurrent2':  ['AC Current S', 'A', 'mdi:flash-outline', DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT],
-    'acoutputcurrent3':  ['AC Current T', 'A', 'mdi:flash-outline', DEVICE_CLASS_CURRENT, STATE_CLASS_MEASUREMENT],
-    'actualpower':       ['AC Output Total Power', 'W', 'mdi:weather-sunny', DEVICE_CLASS_POWER, STATE_CLASS_MEASUREMENT], 
-    'energylastmonth':   ['Energy Last Month', 'kWh', 'mdi:flash-outline', DEVICE_CLASS_ENERGY, STATE_CLASS_TOTAL_INCREASING],
-    'energytoday':       ['Energy Today', 'kWh', 'mdi:flash-outline', DEVICE_CLASS_ENERGY, STATE_CLASS_TOTAL_INCREASING],
-    'energythismonth':   ['Energy This Month', 'kWh', 'mdi:flash-outline', DEVICE_CLASS_ENERGY, STATE_CLASS_TOTAL_INCREASING],
-    'energythisyear':    ['energy This Year', 'kWh', 'mdi:flash-outline', DEVICE_CLASS_ENERGY, STATE_CLASS_TOTAL_INCREASING],
-    'energytotal':       ['Energy Total', 'kWh', 'mdi:flash-outline', DEVICE_CLASS_ENERGY, STATE_CLASS_TOTAL_INCREASING]
-}
 
 def _check_config_schema(conf):
   """ Check if the sensors and attributes are valid. """
@@ -201,13 +157,13 @@ class SolisSensor(Entity):
     return uom
 
   @property
-  def device_class
+  def device_class(self):
     """ Return device class """
     return self._device_class
   
   @property
-  def state_class
-    """ Return state class
+  def state_class(self):
+    """ Return state class """
     return self._state_class
     
   @property
