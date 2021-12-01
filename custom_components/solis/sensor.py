e5de99c68469b89aec4910b9261c246abacb16ba
alceasan <3265883+alceasan@users.noreply.github.com>
2021-12-01 09:35:06 +0100

    Bump version

--- custom_components/solis/sensor.py
+++ custom_components/solis/sensor.py
@@ -47,7 +47,7 @@ from .platform2_portal import PortalConfig as inverter_config
 _LOGGER = logging.getLogger(__name__)
 
 # VERSION
-VERSION = '0.3.3'
+VERSION = '0.3.5'
 
 LAST_UPDATED = 'Last updated'
 
