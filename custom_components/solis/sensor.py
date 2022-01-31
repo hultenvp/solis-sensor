ae63597bc58a03e4f24f5ef49f6c70095fd99d0b
hultenvp <61835400+hultenvp@users.noreply.github.com>
2022-01-31 22:39:18 +0100

    Bump patch version

--- custom_components/solis/sensor.py
+++ custom_components/solis/sensor.py
@@ -44,7 +44,7 @@ from .soliscloud_api import SoliscloudConfig
 _LOGGER = logging.getLogger(__name__)
 
 # VERSION
-VERSION = '2.0.2'
+VERSION = '2.0.3'
 
 LAST_UPDATED = 'Last updated'
 SERIAL = 'Inverter serial'
