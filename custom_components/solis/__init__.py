e7c1eb53831252e08bfcba0b70e4dc796b92bda9
hultenvp <61835400+hultenvp@users.noreply.github.com>
2022-10-07 17:00:12 +0200

    Get setup working

--- custom_components/solis/__init__.py
+++ custom_components/solis/__init__.py
@@ -10,6 +10,7 @@ from homeassistant.config_entries import ConfigEntry, SOURCE_IMPORT
 from homeassistant.const import Platform, ATTR_NAME, CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 from homeassistant.helpers.typing import ConfigType, HomeAssistantType, ServiceCallType
 from homeassistant.util import dt as dt_util
 
@@ -22,7 +23,6 @@ from .const import (
     CONF_SECRET,
     CONF_KEY_ID,
     CONF_PLANT_ID,
-    SERVICE,
 )
 from .ginlong_base import PortalConfig
 from .ginlong_api import GinlongConfig
@@ -36,15 +36,25 @@ PLATFORMS = [Platform.SENSOR]
 
 async def async_setup(hass: HomeAssistantType, config: ConfigType):
     """Set up the Solis component from configuration.yaml."""
-    hass.data.setdefault(DOMAIN, {})
-    if DOMAIN in config:
-        hass.async_create_task(
-            hass.config_entries.flow.async_init(
+    _LOGGER.debug("async_setup")
+#    hass.data.setdefault(DOMAIN, {})
+#    for entry in config:
+#        _LOGGER.debug("%s", entry)
+    if "sensor" not in config:
+        return True
+
+    for entry in config["sensor"]:
+        if DOMAIN.lower() == entry["platform"]:
+            async_create_issue(
+                hass,
                 DOMAIN,
-                context={"source": SOURCE_IMPORT},
-                data=config[DOMAIN],
+                "deprecated_yaml",
+                breaks_in_ha_version="2022.12.0",
+                is_fixable=False,
+                severity=IssueSeverity.WARNING,
+                translation_key="deprecated_yaml",
             )
-        )
+
     return True
 
 
@@ -52,43 +62,44 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry
 ) -> bool:
     """Set up platform from a ConfigEntry."""
+    _LOGGER.debug("async_setup_entry")
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    config = entry.data 
-    #hass.data[DOMAIN][config_entry.entry_id]
-    #session = async_get_clientsession(hass)
+    config = entry.data
 
     portal_domain = config[CONF_PORTAL_DOMAIN]
-    portal_username = config[CONF_USERNAME]
-    portal_password = config[CONF_PASSWORD]
-    portal_key_id = config[CONF_KEY_ID]
-    portal_secret: bytes = bytes(config[CONF_SECRET], 'utf-8')
     portal_plantid = config[CONF_PLANT_ID]
+    portal_username = config[CONF_USERNAME]
     portal_version = config[CONF_PORTAL_VERSION]
 
     portal_config: PortalConfig | None = None
     if portal_version == "ginlong_v2":
+        portal_password = config[CONF_PASSWORD]
         portal_config = GinlongConfig(
             portal_domain, portal_username, portal_password, portal_plantid)
     else:
+        portal_key_id = config[CONF_KEY_ID]
+        portal_secret: bytes = bytes(config[CONF_SECRET], 'utf-8')
         portal_config = SoliscloudConfig(
             portal_domain, portal_username, portal_key_id, portal_secret, portal_plantid)
 
     # Initialize the Ginlong data service.
     service: InverterService = InverterService(portal_config, hass)
-
-    hass.data[DOMAIN][entry.entry_id][SERVICE] = service
+    hass.data[DOMAIN][entry.entry_id] = service
 
     # Forward the setup to the sensor platform.
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, component)
         for component in PLATFORMS
     )
+    # hass.async_create_task(
+    #     hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    # )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
+    _LOGGER.debug("async_unload_entry")
     unload_ok = all(
         await asyncio.gather(
             *[
@@ -98,5 +109,5 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
         )
     )
 
-    hass.data[DOMAIN][entry.entry_id][SERVICE].logout()
+    await hass.data[DOMAIN][entry.entry_id].shutdown()
     return unload_ok
