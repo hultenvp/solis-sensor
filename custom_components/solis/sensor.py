22a6d526f2252e8c74c1f6610c41eba8938c7d70
hultenvp <61835400+hultenvp@users.noreply.github.com>
2022-10-22 21:39:46 +0200

    Bump patch version

--- custom_components/solis/sensor.py
+++ custom_components/solis/sensor.py
@@ -43,7 +43,7 @@ from .service import (ServiceSubscriber, InverterService)
 _LOGGER = logging.getLogger(__name__)
 
 # VERSION
-VERSION = '3.0.2'
+VERSION = '3.0.3'
 
 # ATTRIBUTES
 LAST_UPDATED = 'Last updated'
