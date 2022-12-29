3fca2ca200da59ae8a4c20313438206f91ae4040
hultenvp <61835400+hultenvp@users.noreply.github.com>
2022-12-29 22:37:48 +0100

    Allow plant id override

--- test/apitest_async.py
+++ test/apitest_async.py
@@ -13,9 +13,12 @@ from soliscloud_api.soliscloud_api import *
 
 DELAY = 1 #seconds
 
+plant_ids = None
+inverter_ids = {}
 # Provided by Solis Support
 KeyId = "YOUR KEY GOES HERE"
 secretKey = b'YOUR KEY GOES HERE'
+#plant_ids = [ADD PLANTID LIST HERE IF YOU WANT TO OVERRIDE DETECTION]
 
 async def details_per_station(api, station_ids):
     for sid in station_ids:
@@ -48,7 +51,9 @@ async def collector_details_per_station(api, station_ids):
             print("collector_list(",sid,"): Failed with:",err)
 
 async def inverter_details_per_station(api, station_ids):
+    global inverter_ids
     for sid in station_ids:
+        inverter_ids[sid] = []
         try:
             await asyncio.sleep(DELAY)
             response = await api.inverter_list(KeyId, secretKey, station_id=sid)
@@ -58,6 +63,7 @@ async def inverter_details_per_station(api, station_ids):
                 for record in response:
                     await asyncio.sleep(DELAY)
                     try:
+                        inverter_ids[sid].append(record["sn"])
                         response = await api.inverter_detail(KeyId, secretKey, inverter_sn=record["sn"])
                         if response is not None:
                             print("inverter_detail(station_id =",sid,", inverter_sn =",record["sn"],"): [OK]")
@@ -76,38 +82,95 @@ async def station_graphs(api, station_ids):
                 print("station_day(",sid,"): [OK]")
                 if verbose: print(json.dumps(response,sort_keys=True))
         except SoliscloudAPI.SolisCloudError as err:
-            print("inverter_list(station_id =",sid,"): Failed with:",err)
+            print("station_day(station_id =",sid,"): Failed with:",err)
         try:
             response = await api.station_month(KeyId, secretKey, currency="EUR", month="2022-12", station_id=sid)
             if response is not None:
                 print("station_month(",sid,"): [OK]")
                 if verbose: print(json.dumps(response,sort_keys=True))
         except SoliscloudAPI.SolisCloudError as err:
-            print("inverter_list(station_id =",sid,"): Failed with:",err)
+            print("station_month(station_id =",sid,"): Failed with:",err)
         try:
             response = await api.station_year(KeyId, secretKey, currency="EUR", year="2022", station_id=sid)
             if response is not None:
                 print("station_year(",sid,"): [OK]")
                 if verbose: print(json.dumps(response,sort_keys=True))
         except SoliscloudAPI.SolisCloudError as err:
-            print("inverter_list(station_id =",sid,"): Failed with:",err)
+            print("station_year(station_id =",sid,"): Failed with:",err)
         try:
             response = await api.station_all(KeyId, secretKey, currency="EUR", station_id=sid)
             if response is not None:
                 print("station_all(",sid,"): [OK]")
                 if verbose: print(json.dumps(response,sort_keys=True))
         except SoliscloudAPI.SolisCloudError as err:
-            print("inverter_list(station_id =",sid,"): Failed with:",err)
+            print("station_all(station_id =",sid,"): Failed with:",err)
+
+async def inverter_graphs(api, station_ids):
+    for sid in station_ids:
+        for isn in inverter_ids[sid]:
+            try:
+                await asyncio.sleep(DELAY)
+                response = await api.inverter_day(KeyId, secretKey, currency="EUR", time="2022-12-27", time_zone=1, inverter_sn=isn)
+                if response is not None:
+                    print("inverter_day(",isn,"): [OK]")
+                    if verbose: print(json.dumps(response,sort_keys=True))
+            except SoliscloudAPI.SolisCloudError as err:
+                print("inverter_day(inverter_id =",isn,"): Failed with:",err)
+            try:
+                await asyncio.sleep(DELAY)
+                response = await api.inverter_month(KeyId, secretKey, currency="EUR", month="2022-12", inverter_sn=isn)
+                if response is not None:
+                    print("inverter_month(",isn,"): [OK]")
+                    if verbose: print(json.dumps(response,sort_keys=True))
+            except SoliscloudAPI.SolisCloudError as err:
+                print("inverter_month(inverter_id =",isn,"): Failed with:",err)
+            try:
+                await asyncio.sleep(DELAY)
+                response = await api.inverter_year(KeyId, secretKey, currency="EUR", year="2022", inverter_sn=isn)
+                if response is not None:
+                    print("inverter_year(",isn,"): [OK]")
+                    if verbose: print(json.dumps(response,sort_keys=True))
+            except SoliscloudAPI.SolisCloudError as err:
+                print("inverter_year(inverter_id =",isn,"): Failed with:",err)
+            try:
+                await asyncio.sleep(DELAY)
+                response = await api.inverter_all(KeyId, secretKey, currency="EUR", inverter_sn=isn)
+                if response is not None:
+                    print("inverter_all(",isn,"): [OK]")
+                    if verbose: print(json.dumps(response,sort_keys=True))
+            except SoliscloudAPI.SolisCloudError as err:
+                print("inverter_all(inverter_id =",isn,"): Failed with:",err)
 
 async def call_solis(api):
-    response = await api.user_station_list(KeyId, secretKey)
-    if response is not None:
-        print("user_station_list: [OK]")
-        if verbose: print(json.dumps(response,sort_keys=True))
-    station_ids = [record["id"] for record in response]
-    await details_per_station(api, station_ids)
-    await collector_details_per_station(api, station_ids)
-    await station_graphs(api, station_ids)
+    global plant_ids
+    if plant_ids is None:
+        try:
+            response = await api.user_station_list(KeyId, secretKey)
+            if response is not None:
+                print("user_station_list: [OK]")
+                if verbose: print(json.dumps(response,sort_keys=True))
+                plant_ids = [record["id"] for record in response]
+        except SoliscloudAPI.SolisCloudError as err:
+            print("user_station_list(): Failed with:",err)
+            print("Falling back to station_detail_list")
+            try:
+                response = await api.user_station_list(KeyId, secretKey)
+                if response is not None:
+                    print("station_detail_list: [OK]")
+                    if verbose: print(json.dumps(response,sort_keys=True))
+                    plant_ids = [record["id"] for record in response]
+            except SoliscloudAPI.SolisCloudError as err:
+                print("station__detail_list(): Failed with:",err)
+    else:
+        print("Using predefined station list")
+    if plant_ids is None:
+        print("Cannot retrieve station ID's, giving up")
+        return
+    await details_per_station(api, plant_ids)
+    await collector_details_per_station(api, plant_ids)
+    await inverter_details_per_station(api, plant_ids)
+    await station_graphs(api, plant_ids)
+    await inverter_graphs(api, plant_ids)
 
 async def main():
     loop = asyncio.get_event_loop()
