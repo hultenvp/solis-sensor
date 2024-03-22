7f3117bad010bd6a25d99001576af746b61e19ea
hultenvp <61835400+hultenvp@users.noreply.github.com>
2024-03-22 13:42:29 +0100

    Update patch version

--- custom_components/solis/sensor.py
+++ custom_components/solis/sensor.py
@@ -43,7 +43,7 @@ from .service import (ServiceSubscriber, InverterService)
 _LOGGER = logging.getLogger(__name__)
 
 # VERSION
-VERSION = '3.5.1'
+VERSION = '3.5.2'
 
 # ATTRIBUTES
 LAST_UPDATED = 'Last updated'
