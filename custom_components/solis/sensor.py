efa09e74fcfdc6f15822504b96df8e88b8f48863
P <51826628+patduckless@users.noreply.github.com>
2025-05-04 14:47:01 +0100

    Update sensor.py
    Bump version number to 3.10.0

--- custom_components/solis/sensor.py
+++ custom_components/solis/sensor.py
@@ -30,7 +30,7 @@ from .service import InverterService, ServiceSubscriber
 _LOGGER = logging.getLogger(__name__)
 
 # VERSION
-VERSION = "3.9.2"
+VERSION = "3.10.0"
 
 # ATTRIBUTES
 LAST_UPDATED = "Last updated"
