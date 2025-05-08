ab16062b9100c06405bd786b3f18109734c7eeca
Koen Konst <c.h.konst@avisi.nl>
2025-05-08 10:08:34 +0200

    Bumped patch version to 3.10.1

--- custom_components/solis/sensor.py
+++ custom_components/solis/sensor.py
@@ -43,7 +43,7 @@ from .service import InverterService, ServiceSubscriber
 _LOGGER = logging.getLogger(__name__)
 
 # VERSION
-VERSION = "3.10.0"
+VERSION = "3.10.1"
 
 # ATTRIBUTES
 LAST_UPDATED = "Last updated"
