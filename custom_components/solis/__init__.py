2e9439fb06b70541ec27ea2ea0ea7357a6e3d38a
Bryden Smith <bryden.smith@dynavics.co.uk>
2025-07-31 14:42:18 +0100

    Add refresh interval configuration options for Solis integration

--- custom_components/solis/__init__.py
+++ custom_components/solis/__init__.py
@@ -25,6 +25,8 @@ from .const import (
     CONF_SECRET,
     CONF_USERNAME,
     DOMAIN,
+    CONF_REFRESH_OK,
+    CONF_REFRESH_NOK,
 )
 from .ginlong_api import GinlongConfig
 from .ginlong_base import PortalConfig
@@ -109,7 +111,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         )
 
     # Initialize the Ginlong data service.
-    service: InverterService = InverterService(portal_config, hass)
+    refresh_ok = config[CONF_REFRESH_OK]
+    refresh_error = config[CONF_REFRESH_NOK]
+    service: InverterService = InverterService(portal_config, hass, refresh_ok, refresh_error)
     hass.data[DOMAIN][entry.entry_id] = service
 
     # Forward the setup to the sensor platform.
