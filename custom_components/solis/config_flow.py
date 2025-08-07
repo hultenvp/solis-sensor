5b2a94e6fbc10ef1509eb3e0c9392a757087adb0
Justin.Mason <Justin.Mason@messagegears.com>
2025-08-07 10:53:14 +0100

    Improve config text strings

--- custom_components/solis/config_flow.py
+++ custom_components/solis/config_flow.py
@@ -188,8 +188,8 @@ class SolisConfigFlow(ConfigFlow, domain=DOMAIN):
 
         data_schema = {
             vol.Required(CONF_USERNAME, default=None): cv.string,
-            vol.Required(CONF_SECRET, default="00"): cv.string,
             vol.Required(CONF_KEY_ID, default=""): cv.string,
+            vol.Required(CONF_SECRET, default="00"): cv.string,
             vol.Required(CONF_PLANT_ID, default=None): cv.string,
             vol.Required("Control"): data_entry_flow.section(
                 vol.Schema(
